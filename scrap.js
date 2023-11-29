8df8fc58dc064023aed4bd8d43ce7d2c8c3589f2
akash <akashbhandari0101@gmail.com>
2023-11-30 00:10:34 +0530

    config mail changed

--- scrap.js
+++ scrap.js
@@ -8,51 +8,50 @@ async function run(){
     await page.goto(`https://www.codechef.com/users/${user}`,{ viewport: { width: 1280, height: 720 }});
 
     // heatMap done
-    // const heatArray = await page.evaluate(()=>{
-    //     const data=Array.from(document.querySelectorAll('#js-heatmap svg g g rect')).slice(-60);
-    //     const points=data.map((element)=>{
-    //         const date=element.getAttribute('data-date')
-    //         const count=element.getAttribute('data-count')
-    //         return {'count':count,'date':date}
-    //     }).filter(element => element.count)
-    //     return points
-    // })
+    const heatArray = await page.evaluate(()=>{
+        const data=Array.from(document.querySelectorAll('#js-heatmap svg g g rect')).slice(-60);
+        const points=data.map((element)=>{
+            const date=element.getAttribute('data-date')
+            const count=element.getAttribute('data-count')
+            return {'count':count,'date':date}
+        }).filter(element => element.count)
+        return points
+    })
     // console.log(heatArray)
 
     // questionssolved done
-    // const qusetionsSolved=await page.evaluate(()=> {
-    //      return document.querySelector(`[id^="highcharts-"] > svg > g.highcharts-data-labels.highcharts-series-0.highcharts-pie-series.highcharts-tracker > g.highcharts-label.highcharts-data-label.highcharts-data-label-color-0 > text > tspan`).textContent
-    // })
-    
+    const qusetionsSolved=await page.evaluate(()=> {
+         return document.querySelector(`[id^="highcharts-"] > svg > g.highcharts-data-labels.highcharts-series-0.highcharts-pie-series.highcharts-tracker > g.highcharts-label.highcharts-data-label.highcharts-data-label-color-0 > text > tspan`).textContent
+    })
     // console.log(qusetionsSolved)
 
     //numberofContests done
-    // var numberOfContests = await page.evaluate(()=>{
-    //     return document.querySelector("body > main > div > div > div > div > div > section.rating-graphs.rating-data-section > div.rating-title-container > div > b").textContent
-    // })
+    var numberOfContests = await page.evaluate(()=>{
+        return document.querySelector("body > main > div > div > div > div > div > section.rating-graphs.rating-data-section > div.rating-title-container > div > b").textContent
+    })
     // console.log(numberOfContests)
 
     //last 5 ratings done
-    // const lastFewRatings=await page.evaluate((numberOfContests)=>{
-    //     var objArray=[]
-    //     const graph = window.Highcharts.charts.find((chart) => chart.container.parentNode.id == 'cumulative-graph').series[0]
-    //         for(var i=1;i<=5;i++){
-    //             if(numberOfContests-i<0)
-    //             break;
-    //             graph.points[numberOfContests-i].firePointEvent('click')
-    //             const contestRating=document.querySelector("#cumulative > div.rank-stats > div:nth-child(1) > div > a").textContent
-    //             const contestName=document.querySelector("#rating-box-all > div.contest-name > a").textContent
-    //             const contestDate=document.querySelector("#rating-box-all > div.contest-name > span").textContent
-    //             const contestGlobalRank=document.querySelector("#global-rank-all > strong").textContent
-    //             objArray.push({
-    //                 "contestRating" : contestRating,
-    //                 "contestName" : contestName,
-    //                 "contestDate" : contestDate,
-    //                 "contestGlobalRank" : contestGlobalRank
-    //             })
-    //         }
-    //         return objArray
-    // },numberOfContests)
+    const lastFewRatings=await page.evaluate((numberOfContests)=>{
+        var objArray=[]
+        const graph = window.Highcharts.charts.find((chart) => chart.container.parentNode.id == 'cumulative-graph').series[0]
+            for(var i=1;i<=5;i++){
+                if(numberOfContests-i<0)
+                break;
+                graph.points[numberOfContests-i].firePointEvent('click')
+                const contestRating=document.querySelector("#cumulative > div.rank-stats > div:nth-child(1) > div > a").textContent
+                const contestName=document.querySelector("#rating-box-all > div.contest-name > a").textContent
+                const contestDate=document.querySelector("#rating-box-all > div.contest-name > span").textContent
+                const contestGlobalRank=document.querySelector("#global-rank-all > strong").textContent
+                objArray.push({
+                    "contestRating" : contestRating,
+                    "contestName" : contestName,
+                    "contestDate" : contestDate,
+                    "contestGlobalRank" : contestGlobalRank
+                })
+            }
+            return objArray
+    },numberOfContests)
     // console.log(lastFewRatings)
 
     await browser.close()
